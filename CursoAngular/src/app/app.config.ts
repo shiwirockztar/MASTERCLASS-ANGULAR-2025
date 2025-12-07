099e368f8139e210a5f3ee54fd314f3c19189578
shiwirockztar <68036093+shiwirockztar@users.noreply.github.com>
2025-12-07 05:49:40 +0000

    Estructura completa del proyecto
    Se explicó qué es un bundle y el proceso completo del build:
    - Minification
    - Uglify
    - Bundles
    - Lazy loading
    - Performance
    - Seguridad
    
    También se explicó qué es un lazy chunk: se encarga de renderizar únicamente lo necesario.
    Por ejemplo, en un login solo se renderiza la vista de autenticación mientras el dashboard permanece oculto hasta ser requerido.
    
    Referencia: https://youtu.be/R1QePsia5xk?t=5353

--- CursoAngular/src/app/app.config.ts
+++ CursoAngular/src/app/app.config.ts
@@ -1,16 +1,20 @@
 import { ApplicationConfig, provideZonelessChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     //provideBrowserGlobalErrorListeners(),
     //provideRouter(routes), provideClientHydration(withEventReplay())
+
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes,withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay())
 
   ]
 };
